9749c83535f1d955811aee3678bd79b7199c316a
Philippe Timothe <PhilippeTimothe@gmail.com>
2024-05-08 18:00:05 -0400

    Initial Navbar implemetation."

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import NavigationBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <main className="main">
+      <NavigationBar />
       <h1>Home</h1>
     </main>
   );
